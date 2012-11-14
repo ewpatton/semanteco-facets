a2617cf00e6fbfabda260d20f985f7e373100893
Matthew Ma <mxc0317@gmail.com>
2012-11-14 18:54:06 +0000

    uiteam

--- regulation/src/main/resources/META-INF/res/regulation.js
+++ regulation/src/main/resources/META-INF/res/regulation.js
@@ -37,13 +37,94 @@ $(window).bind("initialize", function() {
 		RegulationModule.queryForSitePollution({}, function(data){
 			$("#spinner").hide();
 			console.log("data retrieved in queryForSitePollution");
+			data=JSON.parse(data);
 			console.log(data);
-
+			console.log(marker);
 			// call the auto-generated AJAX method to get the polluted measurements
 
-			data="<div>THE LOADED DATA DOES NOT CONTAIN ENOUGH DATA TO GENERATE A FORM, USE FAKE DATA STILL</div><table border=1><tr><td>MeasurementId</td><td>monitorSite</td><td>Chemical</td><td>hasValue</td><td>hasUnit</td><td>DateCollected</td><td>RegulationViolation</td></tr><tr><td>AirMeasurement_1a</td><td>01-073-0023</td><td class='chemicals'>SO2</td><td>0.00076</td><td>ppm</td><td>1/1/2012</td><td>no</td></tr><tr><td>AirMeasurement_1b</td><td>01-073-0023</td><td class='chemicals'>CO2</td><td>0.00096</td><td>ppm</td><td>2/1/2012</td><td>yes</td></tr><tr><td>AirMeasurement_1c</td><td>01-073-0023</td><td class='chemicals'>HCl</td><td>0.00086</td><td>ppm</td><td>3/1/2012</td><td>no</td></tr><tr><td>AirMeasurement_1c</td><td>01-073-0023</td><td class='chemicals'>Hg</td><td>0.00096</td><td>ppm</td><td>4/1/2012</td><td>no</td></tr><tr><td>AirMeasurement_1c</td><td>01-073-0023</td><td class='chemicals'>Cl2</td><td>0.00086</td><td>ppm</td><td>5/1/2012</td><td>no</td></tr></table>";
+			//////////////////////////////////////////////////////////////////
+		    var contents = "";
+		    if(marker.data.label.value != '')
+		      contents += "<div class='top'>Site: "+marker.data.label.value+"</div>";
+		  	console.log(data.results);
+		    var bindings = data.results.bindings;
+		    var found = {};
+		    var effects = {};
+		    var effectURLs = {};
+		    if(bindings.length==0) {
+		      contents += "<div class='bottom'>This site has no known pollution based on the regulation you selected.</div>";
+		      marker.tabledata=contents;
+		    }
+		    else{
+		    	contents += "<div class=\"table-wrapper\"><table border=\"1\"><tr><th>Pollutant</th><th>Measured Value</th><th>Limit Value</th><th>Time</th><th>Species</th><th>Health Effects</th></tr>";
+			    var table = $(document.createElement("table"));
+			    for(var i=0;i<bindings.length;i++) {
+			      	// try {
+						var result = bindings[i];
+						var element = result["element"].value;
+						var label = element.substr(element.indexOf("#")+1).replace(/_/g," ");
+						var time = "2012";
+						// if(found[label+time]) 
+						// 	continue;
+						// found[label+time] = true;
+						var value = result["value"].value;
+						var unit = result["unit"].value;
+						if(unit.indexOf("http://")>=0) {
+						  unit = unit.substr(unit.lastIndexOf("/")+1);
+						}
+						var op = result["op"].value;
+						var limit = result["limit"].value;
+						contents += "<tr class=\""+(i%2==0?"even":"odd")+"\"><td class='characteristics' data-value='"+ element +"'>";
+						contents += label+"</td><td>"+value+" "+unit+"<a href=\"javascript:openProvWindow(\'"+element.substring(element.indexOf('#')+1)+"\',\'"+value+"\',\'"+unit+"\',"+ false+","+marker.data.facility.value+",\'"+encodeURIComponent(marker.data.site.value)+"\')\">?</a></td><td>";
+						contents += op+" "+limit+" "+unit+"<a href=\"javascript:openProvWindow(\'"+element.substring(element.indexOf('#')+1)+"\',\'"+limit+"\',\'"+unit+"\',"+ true+")\">?</a></td><td>"+time+"</td>";
+						//var curSpecies=result["species"].value;
+						var curSpecies=result["species"];
+						//alert(curSpecies)
+						contents += "<td>"+curSpecies+"</td>";
+						contents += "<td>";
+						var first = true;
+						// var healthBds= result["health"].results.bindings;
+						/*for(var effect in effects[label]) {
+						  if(!first) contents += ",<br/>";
+						  if(effectURLs[label][effect])
+						  	contents += "<a href=\""+effectURLs[label][effect]+"\">" + effects[label][effect]+"</a>";
+						  else
+						  	contents += effects[label][effect];
+						  first = false;
+						}*/
+				  //   	for(var i=0;i<healthBds.length;i++) {
+				  //   	 if(!first) contents += ",<br/>";
+				  //   	 else
+				  //   	   first = false;
+				  //   	  var healthRes = healthBds[i];
+				  //   	  var effect=healthRes["effect"].value;
+				  //   	  effect=effect.substr(effect.indexOf("#")+1).replace(/_/g," ");
+				  //   	  var effectURL=healthRes["effectURL"].value;
+				  //   	  if(effectURL==null || effectURL.length==0)
+				  //   	    contents += effect;
+				  //   	  else
+				  //   	    contents += "<a href=\""+effectURL+"\">" + effect+"</a>";
+						// }
 
-			marker.tabledata=data;
+			      	// }//end of try
+			      	// catch(e) {
+			      	// 	console.log("catch"); 
+			      	// 	console.log(e);
+			      	// }
+			    }
+			    contents += "</td></tr></table></div>";
+			    // contents += "<div class='bottom'><a href='"+(marker.data.facility.value?"trend/epaTrend.html":"trend/usgsTrend.html")+"?state="+state+"&county="+countyCode+"&site="+encodeURIComponent(marker.siteData.uri)+"' target='_new'>Visualize Characteristics</a></div><div class='bottom'></div>";
+			    // contents += "<div class='bottom'></div><div class='bottom'></div>";
+			    marker.tabledata=contents;
+		    }
+		    
+			////////////////////////////////////////////////////////////
+
+
+			// data="<div>THE LOADED DATA DOES NOT CONTAIN ENOUGH DATA TO GENERATE A FORM, USE FAKE DATA STILL</div><table border=1><tr><td>MeasurementId</td><td>monitorSite</td><td>Chemical</td><td>hasValue</td><td>hasUnit</td><td>DateCollected</td><td>RegulationViolation</td></tr><tr><td>AirMeasurement_1a</td><td>01-073-0023</td><td class='chemicals'>SO2</td><td>0.00076</td><td>ppm</td><td>1/1/2012</td><td>no</td></tr><tr><td>AirMeasurement_1b</td><td>01-073-0023</td><td class='chemicals'>CO2</td><td>0.00096</td><td>ppm</td><td>2/1/2012</td><td>yes</td></tr><tr><td>AirMeasurement_1c</td><td>01-073-0023</td><td class='chemicals'>HCl</td><td>0.00086</td><td>ppm</td><td>3/1/2012</td><td>no</td></tr><tr><td>AirMeasurement_1c</td><td>01-073-0023</td><td class='chemicals'>Hg</td><td>0.00096</td><td>ppm</td><td>4/1/2012</td><td>no</td></tr><tr><td>AirMeasurement_1c</td><td>01-073-0023</td><td class='chemicals'>Cl2</td><td>0.00086</td><td>ppm</td><td>5/1/2012</td><td>no</td></tr></table>";
+
+
+			// marker.tabledata=data;
 			$(window).trigger('pop-infowindow',marker);
 			//THE LOADED DATA DOES NOT CONTAIN ENOUGH DATA TO GENERATE A FORM, USE FAKE DATA STILL
 			
